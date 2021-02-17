554cdfd3cd1a7a121480d39c0ffad388130ab55d
mekabuko <36804514+mekabuko@users.noreply.github.com>
2021-02-17 22:43:22 +0900

    finish Part2

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
+      theme: ThemeData(primaryColor: Colors.white),
       home: RandomWords(),
     );
   }
@@ -24,12 +25,14 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
   final _biggerFont = TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
       ),
       body: _buildSuggestions(),
     );
@@ -53,11 +56,54 @@ class _RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     // 最終的に呼ばれる、文字の表示を行う部分
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
+  // 右上のボタンを押された時の動作
+  void _pushSaved() {
+    // Navigator で移動。
+    Navigator.of(context).push(
+      // ページ
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          //  tiles で、saveされた Pair の一覧を表示する
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(pair.asPascalCase, style: _biggerFont),
+              );
+            },
+          );
+          // ListTile を使って、tileの一覧の表示を作る
+          final divided =
+              ListTile.divideTiles(context: context, tiles: tiles).toList();
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
       ),
     );
   }
